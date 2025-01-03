3908a136a5eb5bf3be20de9db7dbf55f88bfa99c
karan dhole <karan.dhole20@pccoepune.org.>
2025-01-03 14:58:01 +0530

    S

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const jwt  = require("jsonwebtoken");
 
 // Middleware for parsing JSON and handling CORS
 
-app.use(cors({ origin: '*' })); // Replace '*' with your frontend's domain for better security
+app.use(cors({ origin: 'https://localhost:3000' })); // Replace '*' with your frontend's domain for better security
 
 app.use(cors()); 
 app.use(bodyParser.json());
@@ -627,7 +627,7 @@ app.post('/success', (req, res) => {
 
 app.listen(port, (err) => {
     if (!err) {
-    console.log(`Server running on port ${port}`);
+    console.log(`Server running on port `);
   } else {
     console.error("Error starting server:", err);
   }
