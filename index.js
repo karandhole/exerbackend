e26e68bd32da0a4861ff762f2348dd4964dbc5b9
karan dhole <karan.dhole20@pccoepune.org.>
2025-02-04 17:38:59 +0530

    S

--- index.js
+++ index.js
@@ -28,32 +28,16 @@ const jwt  = require("jsonwebtoken");
 
 
 
-// CORS Configuration
-const allowedOrigins = [
-  "http://localhost:3000",   // For local development
-  "https://www.exerenergy.com" // Your production frontend domain
-];
+// app.use(cors({
+//   origin: ["http://localhost:3000", "https://exerenergy.com"], // Add frontend domains here
+//   methods: "GET,POST,PUT,DELETE",
+//   allowedHeaders: "Content-Type,Authorization"
+// }));
 
-app.use(cors({
-  origin: function (origin, callback) {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true); // Allow request from allowed origins
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  methods: "GET,POST,OPTIONS,PUT,DELETE",
-  allowedHeaders: "Content-Type,Authorization", // Customize as necessary
-  credentials: true // Allow credentials like cookies or authorization headers
-}));
-
-// Preflight handling (this is automatically handled by `cors` middleware, but add this if needed)
-app.options("*", cors());  // Preflight CORS
-
-// app.use(cors({ origin: "*" }));
+app.use(cors({ origin: "*" }));
 
 
-// app.use(cors()); 
+app.use(cors()); 
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
